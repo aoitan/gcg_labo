7ff62014d48db2983d843111546db18d8bba0670
あおいたん <aoi@nagatsuki-do.net>
2014-05-08 14:50:15 +0900

    Create idle_test.js

---
+++ api_reading/20140323_using_Idle_API/idle_test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var idle = {
+    time: 3,
+    onidle: function () { output('onidle'); },
+    onactive: function () { output('onactive'); }
+};
+function output(message) {
+    var o = document.getElementById('output');
+    var p = document.createElement('p');
+    p.innerHTML = message;
+    o.appendChild(p);
+    console.log(message);
+}
+function setIdleObserver() {
+    output('setIdleObserver');
+    if (navigator.addIdleObserver) {
+        output('addIdleObserver');
+        navigator.addIdleObserver(idle);
+    }
+}
+function unsetIdleObserver() {
+    output('unsetIdleObserver');
+    if (navigator.removeIdleObserver) {
+        output('removeIdleObserver');
+        navigator.removeIdleObserver(idle);
+    }
+}
+function init() {
+    var addButton = document.getElementById('add');
+    addButton.addEventListener('click', setIdleObserver);
+    var removeButton = document.getElementById('remove');
+    removeButton.removeEventListener('click', unsetIdleObserver);
+}
+
+window.onload = init;
+
